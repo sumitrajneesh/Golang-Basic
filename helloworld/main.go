bbc114adf935e01cacdd0804d68f6cf59693ed2e
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-08-02 21:21:50 +0530

    commit

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,70 @@
-package main
+package main //package should be name main
 
-import "fmt"
+import "fmt" //import statement
+
+// varb(
+// 	actorName string = "sumit"
+// 	companion string = "choudhary"
+// 	doctorNumber int = 3
+// )
+// var(
+// 	counter int = 0
+// )
+
+// var i int = 27
 
 func main() {
-	fmt.Println("hi sumit")
+	// var i int
+	// i = 43
+	// i = 27
+	// var i int = 45
+	// i := 45
+
+	// var j float32 = 27
+	// k := 99.
+	// fmt.Println(i)
+	// fmt.Printf("%v, %T", k, k)
+	// var theHTTP string = "https://google.com"
+	//converions
+
+	// fmt.Println(i)
+	// var i int = 42
+	// fmt.Println(i)
+	// fmt.Printf("%v, %T", j, j)
+
+	// var i int = 43
+	// fmt.Printf("%v , %T\n", i, i)
+
+	// var j float32
+	// j = float32(i)
+	// fmt.Printf("%v , %T\n", j, j)
+	// }
+	// var i int = 43
+	// fmt.Printf("%v , %T\n", i, i)
+
+	// var j string
+	// j = strconv.Itoa(i)
+	// fmt.Printf("%v , %T\n", j, j)
+
+	// var n bool = true
+	// fmt.Printf("%v %T\n", n, n)
+
+	// a := 10
+	// b := 3
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+	// fmt.Println(a % b)
+
+	// var a int = 10
+	// var b int8 = 3
+	// fmt.Println(a + int(b))
+
+	a := 10
+	b := 3
+	fmt.Println(a & b)
+	fmt.Println(a | b)
+	fmt.Println(a ^ b)
+	fmt.Println(a &^ b)
 }
